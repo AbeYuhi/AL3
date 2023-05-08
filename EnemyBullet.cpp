e5eb17515ee6530ef3094b2eb7ef79d71fce0570
AbeYuhi <k022g0032@g.neec.ac.jp>
2023-05-08 15:26:00 +0900

    敵の弾が進行方向を向くように

--- EnemyBullet.cpp
+++ EnemyBullet.cpp
@@ -17,12 +17,19 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector
 	//テクスチャ読み込み
 	textureHandle_ = TextureManager::Load("red.png");
 
+	this->velocity_ = velocity;
+
 	worldTransform_.Initialize();
 
 	worldTransform_.translation_ = position;
 
+	worldTransform_.scale_.x = 0.5f;
+	worldTransform_.scale_.y = 0.5f;
+	worldTransform_.scale_.z = 3.0f;
 
-	this->velocity_ = velocity;
+	worldTransform_.rotation_.y = std::atan2(velocity_.x, velocity_.z);
+	float widthLength = Length({velocity_.x, 0, velocity_.z});
+	worldTransform_.rotation_.x = std::atan2(-velocity_.y, widthLength);
 }
 
 void EnemyBullet::Update() {
